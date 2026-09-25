4f3c0c056ddb389658939f38cc9f3ce6c4492e92
Влад W <zapvlad415@gmail.com>
2026-09-25 15:57:50 +0300

    Apply student B configuration

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ const project = {
   title: "Shared Project",
   status: "draft",
   theme: "light",
-  language: "uk",
+  language: "pl",
   itemsPerPage: 10,
   welcomeMessage: "Welcome",
   dateFormat: "DD.MM.YYYY",
